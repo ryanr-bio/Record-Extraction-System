41cb5f8e1e7bbb553c2c7d7f4fbc25b976a830cd
Ryan <ryanriyas123@gmail.com>
2026-06-03 06:11:56 +0400

    Refactor check_progress and save_progress functions in CheckPointSystem.py to improve progress tracking; update data structure to use completed_ids for better management of completed tasks.

--- JSONExtract/CheckPointSystem.py
+++ JSONExtract/CheckPointSystem.py
@@ -1,14 +1,16 @@
 import json
 
 def check_progress(jsonfile):
-    try: 
+    try:
         with open(jsonfile, "r") as f:
             data = json.load(f)
-            last_comp_id = int(data.get("last_comp_id", 0))
-    except json.JSONDecodeError:
-        last_comp_id = 0
-    return last_comp_id
+            completed = set(data.get("completed_ids", []))
+    except (json.JSONDecodeError, FileNotFoundError):
+        completed = set()
+    return completed
 
 def save_progress(jsonfile, comp_id):
+    completed = check_progress(jsonfile)
+    completed.add(int(comp_id))
     with open(jsonfile, "w") as f:
-        json.dump({"last_comp_id": comp_id}, f)
+        json.dump({"completed_ids": list(completed)}, f)
